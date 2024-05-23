653930fa4482edca739008a0348ef339d1c729a6
Vincent Yeh <difkye247@gmail.com>
2024-05-23 16:46:00 +0800

    add create form page and some styles

--- src/pages/Create/Create.jsx
+++ src/pages/Create/Create.jsx
@@ -1,5 +1,54 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Create.css";
 export default function Create() {
-  return <div>Create</div>;
+  let [title, setTitle] = useState("");
+  let [cookingTime, setCookingTime] = useState("");
+  let [method, setMethod] = useState("");
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(title, cookingTime, method);
+  };
+  return (
+    <div className="create">
+      <h2>Add a Recipe</h2>
+      <form
+        onSubmit={(e) => {
+          handleSubmit(e);
+        }}
+      >
+        <label>
+          <span>Recipe Title:</span>
+          <input
+            type="text"
+            value={title}
+            required
+            onChange={(e) => setTitle(e.target.value)}
+          />
+        </label>
+        <label>
+          <span>Recipe Method</span>
+          <textarea
+            type="text"
+            value={method}
+            required
+            onChange={(e) => {
+              setMethod(e.target.value);
+            }}
+          />
+        </label>
+        <label>
+          <span>Cooking Time</span>
+          <input
+            type="number"
+            value={cookingTime}
+            required
+            onChange={(e) => {
+              setCookingTime(e.target.value);
+            }}
+          />
+        </label>
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
 }
--- src/pages/Recipe/Recipe.jsx
+++ src/pages/Recipe/Recipe.jsx
@@ -1,5 +1,29 @@
 import React from "react";
-
+import { useParams } from "react-router-dom";
+import { useFetch } from "../../hooks/useFetch";
+// styles
+import "./Recipe.css";
 export default function Recipe() {
-  return <div>Recipe</div>;
+  let { id } = useParams();
+  let { data, error, isPending } = useFetch(
+    `http://localhost:3000/recipes/${id}`
+  );
+  return (
+    <div className="recipe">
+      {error && <div className="error">{error}</div>}{" "}
+      {isPending && <div className="loding">Loding...</div>}
+      {data && (
+        <>
+          <h1>{data.title}</h1>
+          <p>take {data.cookingTime} to cook.</p>
+          <ul>
+            {data.ingredients.map((item) => (
+              <li key={item}>{item}</li>
+            ))}
+          </ul>
+          <p>{data.method}</p>
+        </>
+      )}
+    </div>
+  );
 }
